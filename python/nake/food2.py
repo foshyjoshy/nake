c7eaa02d1e02759f91e23890ad6e807069265b35
colyt <colyton@gmail.com>
2020-02-10 23:25:06 +1300

    Working on food

--- python/nake/food2.py
+++ python/nake/food2.py
@@ -1,11 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from board import Board
+from snake import Snake
 import time
 
 import random
-
-
 
 
 
@@ -14,12 +12,19 @@ class FoodGenerator():
 
     def __init__(self, board, pos=None, state=None, seed=None):
         self.randomState = np.random.RandomState(seed=seed)
-        if state is None:
+        if state is not None:
             self.randomState.set_state(state)
 
         self.state = self.randomState.get_state()
         self.board = board
         self._arr = np.zeros([self.size], dtype=np.bool)
+
+        # if pos is not None:
+        #     self.pos = pos
+        # else:
+        #     self.findNext()
+        #
+
 
     @property
     def shape(self):
@@ -31,64 +36,40 @@ class FoodGenerator():
 
     def findNext(self, positions):
         """ Finds the next available position """
+        if isinstance(positions, Snake):
+            positions = positions.positions
+
+        indexes = np.ravel_multi_index(positions.T[::-1], self.shape)
+
         self._arr[:] = True
-        self._arr[positions] = False
-        #np.unravel_index
+        self._arr[indexes] = False
 
-        indexes = np.nonzero(self._arr)[0]
-        idx = random.randint(0, indexes.shape[0] - 1)
+        available_indexes = np.nonzero(self._arr)[0]
+        ridx = random.randint(0, available_indexes.shape[0] - 1)
 
+        # # Check if indexing is correct
+        # c = self._arr.reshape(self.shape)
+        # plt.imshow(c)
+        # plt.show()
 
-        y = int(index/board.width)
-        x = index-(y*board.width)
-
-
-
-gen = FoodGenerator()
-gen.findNext()
-
-
-a = time.time()
-
-#randomState = random.seed(a=None)
-for i in range(1000):
-    random.randint(0, 1000)
-print (time.time()-a)
-
-a = time.time()
-
-randomStateX = np.random.RandomState(seed=None)
-for i in range(1000):
-    randomStateX.randint(0, 1000)
-
-print (time.time()-a)
-
-quit()
+        return np.unravel_index(available_indexes[ridx], self.shape)
 
 
 
-board = Board.fromDims(128, 64)
-numvalues = board.width*board.height
+from board import Board
+from snake import Snake
+import consts
 
-arr = np.zeros([numvalues], dtype=np.bool)
-positions = np.arange(numvalues)
-np.random.shuffle(positions)
-positions = positions[:int(numvalues*0.8)]
-print ("Snake length", positions.shape[0])
+snake = Snake.initializeAtPosition((4,4), direction=consts.Moves.DOWN)
+board = Board.fromDims(6, 6)
+gen = FoodGenerator(board)
 
+im = snake.generatePreviewImage(board)
+for i in range(200):
+    idx = gen.findNext(snake)
+    im[idx]=66
 
-
-a = time.time()
-for i in range(1000):
-    arr[:] = True
-    arr[positions] = False
-    indexes = np.nonzero(arr)[0]
-    index = random.randint(0, indexes.shape[0] - 1)
-    #print (indexes)
-    #break
-    y = int(index/board.width)
-    x = index-(y*board.width)
-
-print (time.time()-a)
-
+plt.imshow(im, vmin=0)
+plt.colorbar()
+plt.show()
 
